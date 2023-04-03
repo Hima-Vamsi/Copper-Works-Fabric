35391a6d2acc00b4a5168dcf2246b285b0163289
Hima-Vamsi <hvkatta@gmail.com>
2023-04-03 19:26:23 +0530

    Add basic files for Copper Works 1.17
    This commit adds the basic files needed to start developing a Minecraft Fabric mod. No features or functionality have been implemented yet, but this commit sets up the project structure for future development.

---
+++ src/main/java/net/vamsi/copper_works/Copper_Works.java
@@ -0,0 +1,14 @@
+package net.vamsi.copper_works;
+
+import net.fabricmc.api.ModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Copper_Works implements ModInitializer {
+	public static final String MOD_ID = "copper_works";
+	public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/vamsi/copper_works/Copper_WorksClient.java
@@ -0,0 +1,10 @@
+package net.vamsi.copper_works;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Copper_WorksClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
